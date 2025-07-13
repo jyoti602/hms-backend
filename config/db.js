2415b8ef819556361e91a2b0b47d878ea911c0e8
jyoti602 <jyotibisht12300@gmail.com>
2025-07-13 13:17:06 +0530

    aded the all api

--- config/db.js
+++ config/db.js
@@ -6,10 +6,14 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306 // fallback to 3306 if not defined
 });
 
 db.connect(err => {
-  if (err) throw err;
+  if (err) {
+    console.error('DB connection failed:', err.message);
+    return;
+  }
   console.log('MySQL Connected');
 });
 
